97cd523332e93489c85fdda9c8ab1b30910a0d77
SamDuga <sam.duga@gmail.com>
2023-09-28 09:09:10 -0500

    init

---
+++ BruteForceReact/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BruteForceReact.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
